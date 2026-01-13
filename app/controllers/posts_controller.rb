8b6f131664e9664e972c3453f6d5b89ac33113c0
Surakshya Gaire <gairesurakshya3@gmail.com>
2026-01-13 22:54:30 +0545

    Devise authentication, email confirmation, posts protection, and Bootstrap UI

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.includes(:user).order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post, notice: "Post created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Post deleted."
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
